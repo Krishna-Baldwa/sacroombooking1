1430b1cee0b302aed48ffeca56a64f1f5c131599
Krishna <krishnabaldwa1101@gmail.com>
2023-02-27 17:17:57 +0530

    changes done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserpageComponent } from './userpage/userpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:UserpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
